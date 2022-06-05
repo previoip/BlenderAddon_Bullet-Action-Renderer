adc040409b3fa60379d963a82dd031bf379d632d
Previo Prakasa <previo.prakasa@gmail.com>
2022-06-05 13:39:56 +0700

    test polar transformation

--- BulletAction_fragment/utils.py
+++ BulletAction_fragment/utils.py
@@ -1,5 +1,7 @@
+import imp
 import bpy
-from mathutils import Vector
+from mathutils import Vector, Matrix, Euler, Quartenion
+from math import radians, sin, cos
 from typing import Union, TypeAlias
 import random
 
@@ -7,28 +9,49 @@ import random
 GenericVector: TypeAlias = Union[list[float, int], tuple[float, int], Vector]
 
 def fast_sha(seed: str) -> str:
-    random.seed(string)
+    random.seed(seed)
     h = random.getrandbits(128)
     return "%032x" % h 
 
-def clear_object_data(obj) -> None:
+def normalize_vector(vector: GenericVector) -> Vector:
+    if sum(vector) == 1:
+        return vector
+    return vector / sum(vector)
+
+def clear_objects_data(objs: list) -> None:
     """ Delete and Clear Objects from Scene-Blendfile """
     scene_copy = bpy.context.copy()
-    scene_copy['selected_objects'] = [item for item in list(bpy.context.scene.objects) if item.name.startswith(prefix)]
+    scene_copy['selected_objects'] = objs
     bpy.ops.object.delete(scene_copy)
 
 def set_object_loc_by_name(name: str, loc: GenericVector = Vector((0, 0, 0))) -> Vector:
     """ Sets object by name in cartesian coordinates, returns final position (Vector) """
     obj = bpy.data.objects[name]
     obj.location = loc
-    return obj.location
+    return Vector(obj.location)
 
 def move_object_loc_by_name(name: str, loc: GenericVector = Vector((0, 0, 0))) -> Vector:
     """ Moves object by name in cartesian coordinates, returns final position (Vector) """
+    loc = Vector(loc)
     obj = bpy.data.objects[name]
-    obj.location = obj.location + Vector(loc)
-    return obj.locationx
+    obj.location += loc
+    return Vector(obj.location)
 
-def rotate_object_from_pivot(name: str, pivot) -> Vector:
+def pivot_object_from_point(name: str, pivot: GenericVector = Vector((0, 0, 0)), axis: GenericVector = Vector((0, 0, 0)), angle_deg: float = .0) -> Vector:
     """ Rotates object along a pivot point and direction, returns final position (Vector) """
-    pass
+    obj = bpy.data.objects[name]
+    _x, _y, _z = list(obj.location)
+    _ax, _ay, _az = list(obj.rotation_euler)
+    _r = sqrt(sum(( (pivot[0] - _x)**2, (pivot[1] - _y)**2, (pivot[2] - _z)**2) ))
+    angle_deg = radians(angle_deg)    
+    axis = Vector(axis)
+
+    _ax, _ay, _az = _ax+angle_deg*axis[0], _ay+angle_deg*axis[1], _az+angle_deg*axis[2]    
+    _x = _r * cos(_ax) + pivot[0]
+    _y = _r * sin(_ay) + pivot[1]
+    _z = _r * cos(_az) + pivot[2]
+    
+    obj.location = (_x, _y, _z)
+    obj.rotation_euler = (_ax, _ay, _az)
+
+    return Vector(obj.location), Vector(obj.rotation_euler)
